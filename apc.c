32a70b3cd67c30a7386e969560303d81c455f16f
pgnmirror <vacacax16@gmail.com>
2025-11-01 12:39:23 +0300

    apc: update apc.c to 1.2-1, repo: Update APC packages to 1.2-1!

--- apc.c
+++ apc.c
@@ -42,7 +42,7 @@ int file_exists(const char *path) {
 }
 
 void check_apc_installed() {
-    if (!file_exists("/usr/bin/apc") || !file_exists("/bin/apc")) {
+    if (!file_exists("/usr/bin/apc") && !file_exists("/bin/apc")) {
         printf("APC isn't installed!!! Please install it by the command: apc setup\n");
         exit(1);
     }
@@ -50,10 +50,8 @@ void check_apc_installed() {
 
 void setup_apc() {
     system("pacman -S wget"); // install wget
-    if (system("cp /usr/bin/apc /bin/apc && chmod 755 /bin/apc") == 0)
-        return;
-
-    system("cp /bin/apc /usr/bin/apc && chmod 755 /usr/bin/apc");
+    if (system("cp /usr/bin/apc /bin/apc && chmod 755 /bin/apc") != 0)
+        system("cp /bin/apc /usr/bin/apc && chmod 755 /usr/bin/apc");
 }
 
 int url_exists(const char *pkgname) {
@@ -96,6 +94,57 @@ void delete_package(const char *pkgname) {
     printf("Done!\n");
 }
 
+void update_package(int argc, char *argv[]) {
+    check_root();
+
+    const char *pkg_to_update;
+
+    if (argc > 2) {
+        pkg_to_update = argv[2];
+    } else {
+        pkg_to_update = (sizeof(void*) == 8) ? "apc" : "apc32";
+    }
+
+    char url[512];
+    snprintf(url, sizeof(url), "%s/%s", BASE_URL, pkg_to_update);
+
+    char cmd[512];
+    snprintf(cmd, sizeof(cmd), "wget --spider -q %s", url);
+    if (system(cmd) != 0) {
+        printf("Package '%s' not found on server!\n", pkg_to_update);
+        return;
+    }
+
+    char choice[4];
+    printf("Are you sure to update '%s' from %s? (Y/n): ", pkg_to_update, url);
+    if (!fgets(choice, sizeof(choice), stdin)) exit(1);
+    choice[strcspn(choice, "\n")] = 0;
+
+    if (!(strcmp(choice, "Y") == 0 || strcmp(choice, "y") == 0 || choice[0] == '\0')) {
+        printf("Update canceled.\n");
+        return;
+    }
+
+    char tmpfile[256];
+    snprintf(tmpfile, sizeof(tmpfile), "/tmp/%s", pkg_to_update);
+    snprintf(cmd, sizeof(cmd), "wget -q -O %s %s", tmpfile, url);
+    printf("Downloading %s...\n", pkg_to_update);
+    if (system(cmd) != 0) {
+        printf("Failed to download %s!\n", pkg_to_update);
+        return;
+    }
+
+    snprintf(cmd, sizeof(cmd), "cp %s /usr/bin/%s && chmod 755 /usr/bin/%s", tmpfile, pkg_to_update, pkg_to_update);
+    system(cmd);
+    snprintf(cmd, sizeof(cmd), "cp %s /bin/%s && chmod 755 /bin/%s", tmpfile, pkg_to_update, pkg_to_update);
+    system(cmd);
+
+    snprintf(cmd, sizeof(cmd), "rm -f %s", tmpfile);
+    system(cmd);
+
+    printf("Package '%s' updated successfully!\n", pkg_to_update);
+}
+
 int main(int argc, char *argv[]) {
     check_root();
 
@@ -104,12 +153,12 @@ int main(int argc, char *argv[]) {
         printf("  %s setup\n", argv[0]);
         printf("  %s install <pkg1> [<pkg2> ...]\n", argv[0]);
         printf("  %s delete <pkgname>\n", argv[0]);
+        printf("  %s update [<pkgname>]\n", argv[0]);
         return 1;
     }
 
     if (strcmp(argv[1], "setup") == 0) {
         char self_path[PATH_MAX];
-
         if (realpath(argv[0], self_path) == NULL) {
             char cwd[PATH_MAX];
             if (getcwd(cwd, sizeof(cwd)) != NULL) {
@@ -120,16 +169,16 @@ int main(int argc, char *argv[]) {
                 return 1;
             }
         }
-
         setup_apc();
-    } else if (strcmp(argv[1], "install") == 0) {
+    }
+    else if (strcmp(argv[1], "install") == 0) {
         check_apc_installed();
         if (argc < 3) {
             printf("Usage: %s install <pkg1> [<pkg2> ...]\n", argv[0]);
             return 1;
         }
 
-        printf("Starting APC v1.0-r1...\n\nHere's the all packets i found:\n\n");
+        printf("Starting APC v1.2-1...\n\nHere's the all packets i found:\n\n");
         for (int i = 2; i < argc; i++) {
             if (url_exists(argv[i])) {
                 printf("%s\n", argv[i]);
@@ -147,20 +196,23 @@ int main(int argc, char *argv[]) {
             for (int i = 2; i < argc; i++) {
                 install_package(argv[i]);
             }
-        } else if (strcmp(choice, "N") == 0 || strcmp(choice, "n") == 0) {
-            exit(0);
         } else {
-            printf("It's not a choice.\n");
-            exit(1);
+            printf("Installation canceled.\n");
+            exit(0);
         }
-    } else if (strcmp(argv[1], "delete") == 0) {
+    }
+    else if (strcmp(argv[1], "delete") == 0) {
         check_apc_installed();
         if (argc != 3) {
             printf("Usage: %s delete <pkgname>\n", argv[0]);
             return 1;
         }
         delete_package(argv[2]);
-    } else {
+    }
+    else if (strcmp(argv[1], "update") == 0) {
+        update_package(argc, argv);
+    }
+    else {
         printf("Unknown command!\n");
         return 1;
     }
